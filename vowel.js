3ca53ae2312a3049406135b2b3aea5412d47e55a
KANITA VERMA <vermakanita13@gmail.com>
2024-10-24 23:03:00 +0530

    Add files via upload

---
+++ vowel.js
@@ -0,0 +1,9 @@
+function vowels(){
+    let str="kanita";
+    for(let i=0;i<str.length;i++){
+         c=char.At(i);
+    if(str=='a'&& str=='e'&& str=='i'&& str=='o'&& str=='u'){
+        console.log(i);
+    }
+}}
+vowels();
