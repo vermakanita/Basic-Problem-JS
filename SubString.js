dc5e90c608d863af55039a4025d3be78db10ddfe
KANITA VERMA <vermakanita13@gmail.com>
2024-10-25 10:18:18 +0530

    Add files via upload

---
+++ SubString.js
@@ -0,0 +1,17 @@
+function substring(){
+    
+    let str="kanita verma";
+    let i;
+    let str1="";
+    for(i=0;i<str.length;i++){
+        
+    
+    if(str1==" "){
+        console.log(str1);
+        
+    }
+    }
+    console.log(str1[i]);
+
+}
+substring();
